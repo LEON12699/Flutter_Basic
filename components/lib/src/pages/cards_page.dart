827e2baf0a7fab380fdab031f93b48d3e31516cd
israel leon <israel.leon@ioet.com>
2022-09-21 17:13:28 -0500

    Add Loading image

--- components/lib/src/pages/cards_page.dart
+++ components/lib/src/pages/cards_page.dart
@@ -14,6 +14,8 @@ class CardPage extends StatelessWidget {
           padding: EdgeInsets.all(10.0),
           children: <Widget>[
             _cardTipo1(),
+            SizedBox(height: 30.0),
+            _cardTipo2(),
           ],
         ));
   }
@@ -25,9 +27,7 @@ class CardPage extends StatelessWidget {
           leading: const Icon(Icons.photo_album, color: Colors.blue),
           title: const Text('Soy el titulo de esta tarjeta'),
           subtitle: const Text('Aqui estamos con la descripcion')),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
+      Row(mainAxisAlignment: MainAxisAlignment.end, children: [
         TextButton(
             onPressed: () {},
             child:
@@ -36,6 +36,23 @@ class CardPage extends StatelessWidget {
             onPressed: () {},
             child: const Text('Ok', style: TextStyle(color: Colors.blue)))
       ]),
+    ]));
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+        child: Column(children: <Widget>[
+      FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+          height: 300.00,
+          fit: BoxFit.cover,
+          image: NetworkImage(
+              'https://www.elsalvador.com/wp-content/uploads/2019/10/20191010_1.jpg')),
+      //Image(
+      //    image: NetworkImage(
+      //        'https://www.elsalvador.com/wp-content/uploads/2019/10/20191010_1.jpg')),
+      Container(padding: EdgeInsets.all(10.0), child: Text('texto imagen'))
     ]));
   }
 }
