c86a2e2f4cf9c9ac3565a7406a2e2121a1d445a5
israel leon <israel.leon@ioet.com>
2022-09-21 17:59:50 -0500

    Generate alert widget

--- components/lib/src/pages/alert_page.dart
+++ components/lib/src/pages/alert_page.dart
@@ -7,11 +7,48 @@ class AlertPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Alert Page')),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () => _mostrarAlert(context),
+            child: const Text('Mostrar Alerta'),
+            style: ElevatedButton.styleFrom(
+                primary: Colors.red, shape: StadiumBorder())),
+      ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             Navigator.pop(context);
           },
           child: Icon(Icons.add_location)),
     );
+  }
+
+  _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: const Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const <Widget>[
+                Text('Este es el contenido de la caja de la alerta'),
+                FlutterLogo(size: 100.0)
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('Cancelar')),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Ok'))
+            ],
+          );
+        });
   }
 }
