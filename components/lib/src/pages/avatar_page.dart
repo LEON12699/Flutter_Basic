3ba9a18db54847535bc47ad8c33ce19853557884
israel leon <israel.leon@ioet.com>
2022-09-21 18:20:32 -0500

    Add animated container

--- components/lib/src/pages/avatar_page.dart
+++ components/lib/src/pages/avatar_page.dart
@@ -6,7 +6,31 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Avatar Page')),
+      appBar: AppBar(
+        title: const Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+                radius: 20.0),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+          child: FadeInImage(
+              fadeInDuration: Duration(milliseconds: 200),
+              image: NetworkImage(
+                  'https://images.pexels.com/photos/6777376/pexels-photo-6777376.jpeg'),
+              placeholder: AssetImage('assets/jar-loading.gif'))),
     );
   }
 }
